7baf1add5ea2209b987854b5fc77af98a0adb2c0
Matthew <alexandermatthew12@gmail.com>
2024-04-12 11:31:47 +0800

    feat: price fetching cache algorithm

---
+++ app/utils/price.ts
@@ -0,0 +1,23 @@
+import { fetchTokenPrices } from './binance';
+
+const INTERVAL_MS = 3_000;
+
+// Crypto tickers to keep in cache for price fetching
+const TICKERS = [
+    'BTC',
+    'ETH',
+    'SOL'
+];
+
+// Cache for token prices
+let PRICE_CACHE: Record<string, number> = {};
+
+const pollPrices = async () => {
+    PRICE_CACHE = await fetchTokenPrices(TICKERS)
+    // Set interval
+    setInterval(async () => {
+        PRICE_CACHE = await fetchTokenPrices(TICKERS)
+    }, INTERVAL_MS);
+}
+
+export { pollPrices, PRICE_CACHE }
