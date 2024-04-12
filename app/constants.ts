12158ed4aeff76e3c976c15a8ce02be124d9a62d
YJ <cyberform.jys@gmail.com>
2024-04-12 16:10:16 +0800

    degen pengu

--- app/constants.ts
+++ app/constants.ts
@@ -1,4 +1,4 @@
-const WELCOME_GIF = 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZGpyNHA2YmFsN2hkeHdsb291eWZoZTM1OGI3bnZrNnE4cWg5N3FjeCZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/SBYJYji4vxQIw/giphy.gif'
+const WELCOME_GIF = 'https://media.giphy.com/media/v1.Y2lkPTc5MGI3NjExZDdqemlxOTBvMjE0a2VnczhvbzlxNXJzM2V3NnE0aWRrNnJ6ZDB5bSZlcD12MV9pbnRlcm5hbF9naWZfYnlfaWQmY3Q9Zw/dYHFEFJEt4cYkOVkK3/giphy.gif'
 
 export {
     WELCOME_GIF
