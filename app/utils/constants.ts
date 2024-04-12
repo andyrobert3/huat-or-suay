f751e60995340792f816298190cac5dbac48f126
Matthew <alexandermatthew12@gmail.com>
2024-04-12 11:18:17 +0800

    chore: game status & position

---
+++ app/utils/constants.ts
@@ -0,0 +1,16 @@
+enum GameStatus {
+    NOT_STARTED = 'NOT_STARTED',
+    IN_PROGRESS = 'IN_PROGRESS',
+    WON = 'WON',
+    LOST = 'LOST'
+}
+
+enum PositionType {
+    LONG = 'LONG',
+    SHORT = 'SHORT'
+}
+
+export {
+    GameStatus,
+    PositionType,
+}
